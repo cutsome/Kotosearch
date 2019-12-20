4a7d6c0bd0a8d700721ea7a21355201cdf0f441f
cutsome <nissikpu@gmail.com>
2019-12-21 01:48:49 +0900

    production.rb修正

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -24,7 +24,7 @@
   preload_app true
 
   before_exec do |server|
-    ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
+    ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "Gemfile")
   end
 
 #fork前に行うことを定義。後述
