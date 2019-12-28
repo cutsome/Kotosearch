f12001883000bae78373f432e30d96dd331ae5c2
cutsome <nissikpu@gmail.com>
2019-12-28 11:58:25 +0900

    deploy.rb編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,6 +71,17 @@ namespace :deploy do
     end
   end
 
+  desc 'Reset database'
+  task :db_reset do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:migrate:reset'
+        end
+      end
+    end
+  end
+
   after :publishing, :restart
 
   after :restart, :clear_cache do
